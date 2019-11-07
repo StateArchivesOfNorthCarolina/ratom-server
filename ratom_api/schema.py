3b1287c9d160582a7efd0a7085a76dd40206db6d
Christopher Dixon <cdixon@caktusgroup.com>
2019-11-07 10:30:05 -0500

    linting, removing templatized project.travis.yml file

--- ratom_api/schema.py
+++ ratom_api/schema.py
@@ -1,10 +1,13 @@
 import graphene
 
+
 class Query(graphene.ObjectType):
-  pass
+    pass
+
 
 class Mutation(graphene.ObjectType):
-  pass
+    pass
+
 
 # remove the parameters for the server to start
 # will not start correctly because we don't have any schema!
