b7122e5d921479c07f79c71ee48a13b1ac991b3f
Michael Ashton <michael.ashton@caktusgroup.com>
2019-11-07 12:10:19 -0500

    basic collection query

--- ratom_api/urls.py
+++ ratom_api/urls.py
@@ -19,13 +19,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
-from .schema import schema
+from .schema import main_schema
+
 
 urlpatterns = [
     re_path(r'^$', TemplateView.as_view(template_name='base.html')),
     path('admin', admin.site.urls),
-    path('graphql', GraphQLView.as_view(graphiql=True, schema=schema)),
+    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=main_schema))),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
