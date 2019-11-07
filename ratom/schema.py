55119feb52d0a6f2294704c2e3cf8fbcf747b402
Michael Ashton <michael.ashton@caktusgroup.com>
2019-11-07 12:51:09 -0500

    addds relasy thbo

--- ratom/schema.py
+++ ratom/schema.py
@@ -1,40 +1,21 @@
 import graphene
 
+from graphene import relay, ObjectType
 from graphene_django import DjangoObjectType
-# from graphene_django.filter import DjangoFilterConnectionField
+from graphene_django.filter import DjangoFilterConnectionField
 
 from .models import Collection, Processor, Message
 
-class CollectionType(DjangoObjectType):
+class CollectionNode(DjangoObjectType):
     class Meta: 
         model = Collection
         filter_fields = {
             "title": ["exact", "icontains", "istartswith"],
-            "accessionDate": ["exact", "gte", "lte"]
+            "accession_date": ["exact", "gte", "lte"]
         }
+        interfaces = (relay.Node, )
 
 
 class Query(object):
-    collection = graphene.Field(CollectionType, id=graphene.ID(), title=graphene.String(), accession_date=graphene.Date())
-    all_collections = graphene.List(CollectionType)
-
-    def resolve_collection(self, info, **kwargs):
-        id = kwargs.get("id")
-        title = kwargs.get("title")
-        accession_date = kwargs.get("accession_date")
-        
-        import pdb; pdb.set_trace()
-
-        if id is not None:
-            return Collection.objects.get(pk=id)
-        
-        if title is not None:
-            return Collection.objects.get(title=title)
-
-        if accession_date is not None:
-            return Collection.objects.get(accession_date=accession_date)
-
-        return None
-
-    def resolve_all_collections(self, info, **kwargs):
-        return Collection.objects.all()
+    collection = relay.Node.Field(CollectionNode)
+    all_collections = DjangoFilterConnectionField(CollectionNode)
