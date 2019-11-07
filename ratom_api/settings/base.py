e4018de9478b2649cad94af32b0c1ec739b9a0db
Tobias McNulty <tobias@caktusgroup.com>
2019-11-07 13:19:40 -0500

    Revert "also log django errors to console" (just logs everything twice, still no traceback)
    This reverts commit ac99c1281810c4aa46612388ca4df251c463f2ec.

--- ratom_api/settings/base.py
+++ ratom_api/settings/base.py
@@ -129,12 +129,12 @@ LOGGING = {
     },
     'loggers': {
         'django.request': {
-            'handlers': ['mail_admins', 'console'],
+            'handlers': ['mail_admins'],
             'level': 'ERROR',
             'propagate': True,
         },
         'django.security': {
-            'handlers': ['mail_admins', 'console'],
+            'handlers': ['mail_admins'],
             'level': 'ERROR',
             'propagate': True,
         },
