335445ee78bddf596b8184ab35f2708992b3dea0
Colin Copeland <copelco@caktusgroup.com>
2019-11-08 10:45:38 -0500

    add Message to graphql

--- ratom/schema.py
+++ ratom/schema.py
@@ -7,7 +7,7 @@ from graphene_django.filter import DjangoFilterConnectionField
 from .models import Collection, Processor, Message
 
 class CollectionNode(DjangoObjectType):
-    class Meta: 
+    class Meta:
         model = Collection
         filter_fields = {
             "title": ["exact", "icontains", "istartswith"],
@@ -16,6 +16,21 @@ class CollectionNode(DjangoObjectType):
         interfaces = (relay.Node, )
 
 
+class MessageNode(DjangoObjectType):
+    class Meta:
+        model = Message
+        filter_fields = {
+            "message_id": ["exact", "icontains", "istartswith"],
+            "sent_date": ["exact", "icontains", "istartswith"],
+            "msg_from": ["exact", "icontains", "istartswith"],
+            "msg_to": ["exact", "icontains", "istartswith"],
+            "msg_subject": ["exact", "icontains", "istartswith"],
+        }
+        interfaces = (relay.Node, )
+
+
 class Query(object):
     collection = relay.Node.Field(CollectionNode)
     all_collections = DjangoFilterConnectionField(CollectionNode)
+    message = relay.Node.Field(MessageNode)
+    all_messages = DjangoFilterConnectionField(MessageNode)
