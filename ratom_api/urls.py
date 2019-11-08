b43a23743a42aa231f4aa7f37d016637a3be228e
Colin Copeland <copelco@caktusgroup.com>
2019-11-08 10:50:31 -0500

    re-add csrf_exempt

--- ratom_api/urls.py
+++ ratom_api/urls.py
@@ -25,7 +25,7 @@ from .schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql', GraphQLView.as_view(graphiql=True, schema=schema)),
+    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
