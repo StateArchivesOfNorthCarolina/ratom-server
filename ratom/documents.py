1b014c6612f8919bc9e28988288d894556fa95ae
Colin Copeland <copelco@caktusgroup.com>
2019-11-28 10:48:38 -0500

    first pass at MessageDocument

---
+++ ratom/documents.py
@@ -0,0 +1,38 @@
+from django_elasticsearch_dsl import Document, fields
+from django_elasticsearch_dsl.registries import registry
+from .models import Message
+
+
+@registry.register_document
+class MessageDocument(Document):
+    collection = fields.ObjectField(
+        properties={"title": fields.TextField(), "accession_date": fields.DateField()}
+    )
+
+    class Index:
+        # Name of the Elasticsearch index
+        name = "message"
+        # See Elasticsearch Indices API reference for available settings
+        settings = {"number_of_shards": 1, "number_of_replicas": 0}
+
+    class Django:
+        model = Message  # The model associated with this Document
+
+        # The fields of the model you want to be indexed in Elasticsearch
+        fields = [
+            "msg_from",
+            "msg_subject",
+            "msg_body",
+            "directory",
+        ]
+
+        # Ignore auto updating of Elasticsearch when a model is saved
+        # or deleted:
+        # ignore_signals = True
+
+        # Don't perform an index refresh after every update (overrides global setting):
+        # auto_refresh = False
+
+        # Paginate the django queryset used to populate the index with the specified size
+        # (by default it uses the database driver's default setting)
+        # queryset_pagination = 5000
