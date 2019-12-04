c482813c32a3962ed20b78a17bea87d7cedfaaae
Michael Ashton <michael.ashton@caktusgroup.com>
2019-12-04 16:12:51 -0500

    pre-committed

--- ratom/documents.py
+++ ratom/documents.py
@@ -1,49 +1,98 @@
-from typing import List
+# from typing import List
 
-from django_elasticsearch_dsl import Document, fields
-from django_elasticsearch_dsl.registries import registry
+# from django_elasticsearch_dsl import Document, fields
+# from django_elasticsearch_dsl.registries import registry
+# from .models import Message
+
+
+# @registry.register_document
+# class MessageDocument(Document):
+#     collection = fields.ObjectField(
+#         properties={"title": fields.TextField(), "accession_date": fields.DateField()}
+#     )
+#     labels = fields.KeywordField(multi=True)
+#     # Investigate setting these to text fields
+#     msg_from = fields.KeywordField(multi=True)
+#     directory = fields.KeywordField()
+
+# def prepare_labels(self, instance: Message) -> List[str]:
+#     labels: List[str] = []
+#     if instance.data:
+#         labels = list(instance.data.get("labels", []))
+#     return labels
+
+#     class Index:
+#         # Name of the Elasticsearch index
+#         name = "message"
+#         # See Elasticsearch Indices API reference for available settings
+#         settings = {"number_of_shards": 1, "number_of_replicas": 0}22
+
+#     class Django:
+#         model = Message  # The model associated with this Document
+
+#         # The fields of the model you want to be indexed in Elasticsearch
+#         fields = [
+#             "msg_subject",
+#             "msg_body",
+#             "sent_date",
+#         ]
+
+# Ignore auto updating of Elasticsearch when a model is saved
+# or deleted:
+# ignore_signals = True
+
+# Don't perform an index refresh after every update (overrides global setting):
+# auto_refresh = False
+
+# Paginate the django queryset used to populate the index with the specified size
+# (by default it uses the database driver's default setting)
+# queryset_pagination = 5000
+
+from datetime import datetime
+from elasticsearch_dsl import (
+    Document,
+    Date,
+    Nested,
+    Boolean,
+    analyzer,
+    InnerDoc,
+    Completion,
+    Keyword,
+    Text,
+)
+
 from .models import Message
 
+html_strip = analyzer(
+    "html_strip",
+    tokenizer="standard",
+    filter=["standard", "lowercase", "stop", "snowball"],
+    char_filter=["html_strip"],
+)
 
-@registry.register_document
+
+class NestedCollection(InnerDoc):
+    title = Text()
+    accession_date = Date()
+
+    def age(self):
+        return datetime.now() - self.accession_date
+
+
 class MessageDocument(Document):
-    collection = fields.ObjectField(
-        properties={"title": fields.TextField(), "accession_date": fields.DateField()}
-    )
-    labels = fields.KeywordField(multi=True)
-    # Investigate setting these to text fields
-    msg_from = fields.KeywordField()
-    directory = fields.KeywordField()
+    msg_from = Text()
+    directory = Text()
+    labels = Text(fields={"raw": Keyword()})
+    collection = Nested(NestedCollection)
 
-    def prepare_labels(self, instance: Message) -> List[str]:
-        labels: List[str] = []
+    def prepare_labels(self, instance: Message):
         if instance.data:
             labels = list(instance.data.get("labels", []))
         return labels
 
     class Index:
-        # Name of the Elasticsearch index
         name = "message"
-        # See Elasticsearch Indices API reference for available settings
-        settings = {"number_of_shards": 1, "number_of_replicas": 0}
 
-    class Django:
-        model = Message  # The model associated with this Document
-
-        # The fields of the model you want to be indexed in Elasticsearch
-        fields = [
-            "msg_subject",
-            "msg_body",
-            "sent_date",
-        ]
-
-        # Ignore auto updating of Elasticsearch when a model is saved
-        # or deleted:
-        ignore_signals = True
-
-        # Don't perform an index refresh after every update (overrides global setting):
-        # auto_refresh = False
-
-        # Paginate the django queryset used to populate the index with the specified size
-        # (by default it uses the database driver's default setting)
-        # queryset_pagination = 5000
+    # def save(self, ** kwargs):
+    #     self.created_at = datetime.now()
+    #     return super().save(** kwargs)
--- ratom/schema.py
+++ ratom/schema.py
@@ -1,6 +1,6 @@
 import graphene
 
-from graphene import relay, ObjectType
+from graphene import Node
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
@@ -29,41 +29,32 @@ from .models import Collection, Processor, Message
 from .documents import MessageDocument
 
 
-class CollectionNode(DjangoObjectType):
-    class Meta:
-        model = Collection
-        filter_fields = {
-            "title": ["exact", "iexact", "icontains", "istartswith", "iendswith"],
-            "accession_date": ["exact", "iexact", "icontains", "gte", "lte", "range"],
-        }
-        interfaces = (relay.Node,)
+# class CollectionNode(DjangoObjectType):
+#     class Meta:
+#         model = Collection
+#         filter_fields = {
+#             "title": ["exact", "iexact", "icontains", "istartswith", "iendswith"],
+#             "accession_date": ["exact", "iexact", "icontains", "gte", "lte", "range"],
+#         }
+#         interfaces = (Node,)
 
 
 class MessageNode(ElasticsearchObjectType):
     class Meta:
         document = MessageDocument
-        interfaces = (relay.Node,)
+        interfaces = (Node,)
         filter_backends = [
             FilteringFilterBackend,
             SearchFilterBackend,
-            HighlightFilterBackend,
-            OrderingFilterBackend,
+            # HighlightFilterBackend,
+            # OrderingFilterBackend,
             DefaultOrderingFilterBackend,
         ]
 
         # For `FilteringFilterBackend` backend
         filter_fields = {
-            # The dictionary key (in this case `title`) is the name of
-            # the corresponding GraphQL query argument. The dictionary
-            # value could be simple or complex structure (in this case
-            # complex). The `field` key points to the `title.raw`, which
-            # is the field name in the Elasticsearch document
-            # (`PostDocument`). Since `lookups` key is provided, number
-            # of lookups is limited to the given set, while term is the
-            # default lookup (as specified in `default_lookup`).
-            "Sender": {
+            "msg_from": {
                 "field": "msg_from",
-                # Available lookups
                 "lookups": [
                     LOOKUP_FILTER_TERM,
                     LOOKUP_FILTER_TERMS,
@@ -75,27 +66,7 @@ class MessageNode(ElasticsearchObjectType):
                 # Default lookup
                 "default_lookup": LOOKUP_FILTER_TERM,
             },
-            # The dictionary key (in this case `category`) is the name of
-            # the corresponding GraphQL query argument. Since no lookups
-            # or default_lookup is provided, defaults are used (all lookups
-            # available, term is the default lookup). The dictionary value
-            # (in this case `category.raw`) is the field name in the
-            # Elasticsearch document (`PostDocument`).
-            # "category": "category",
-            # The dictionary key (in this case `tags`) is the name of
-            # the corresponding GraphQL query argument. Since no lookups
-            # or default_lookup is provided, defaults are used (all lookups
-            # available, term is the default lookup). The dictionary value
-            # (in this case `tags.raw`) is the field name in the
-            # Elasticsearch document (`PostDocument`).
-            "NER Tags": "labels",
-            # The dictionary key (in this case `num_views`) is the name of
-            # the corresponding GraphQL query argument. Since no lookups
-            # or default_lookup is provided, defaults are used (all lookups
-            # available, term is the default lookup). The dictionary value
-            # (in this case `num_views`) is the field name in the
-            # Elasticsearch document (`PostDocument`).
-            # "num_views": "num_views",
+            "labels": "labels",
         }
 
         # For `SearchFilterBackend` backend
@@ -106,49 +77,54 @@ class MessageNode(ElasticsearchObjectType):
         }
 
         # For `OrderingFilterBackend` backend
-        ordering_fields = {
-            # The dictionary key (in this case `tags`) is the name of
-            # the corresponding GraphQL query argument. The dictionary
-            # value (in this case `tags.raw`) is the field name in the
-            # Elasticsearch document (`PostDocument`).
-            "Message Sent": "sent_date",
-            # The dictionary key (in this case `created_at`) is the name of
-            # the corresponding GraphQL query argument. The dictionary
-            # value (in this case `created_at`) is the field name in the
-            # Elasticsearch document (`PostDocument`).
-            # "created_at": "created_at",
-            # The dictionary key (in this case `num_views`) is the name of
-            # the corresponding GraphQL query argument. The dictionary
-            # value (in this case `num_views`) is the field name in the
-            # Elasticsearch document (`PostDocument`).
-            # "num_views": "num_views",
-        }
+        # ordering_fields = {
+        #     "Message Sent": "sent_date",
+        # }
 
         # For `DefaultOrderingFilterBackend` backend
-        ordering_defaults = (
-            "-sent_date",  # Field name in the Elasticsearch document
-            # "title.raw",  # Field name in the Elasticsearch document
-        )
+        # ordering_defaults = (
+        #     "-sent_date",  # Field name in the Elasticsearch document
+        # )
 
         # For `HighlightFilterBackend` backend
-        highlight_fields = {
-            "msg_subject": {
-                "enabled": True,
-                "options": {"pre_tags": ["<b>"], "post_tags": ["</b>"],},
-            },
-            "msg_body": {
-                "options": {
-                    "fragment_size": 50,
-                    "number_of_fragments": 1,
-                    "pre_tags": ["<b>"],
-                    "post_tags": ["</b>"],
-                }
-            },
-        }
+        # highlight_fields = {
+        #     "msg_subject": {
+        #         "enabled": True,
+        #         "options": {"pre_tags": ["<b>"], "post_tags": ["</b>"],},
+        #     },
+        #     "msg_body": {
+        #         "options": {
+        #             "fragment_size": 50,
+        #             "number_of_fragments": 1,
+        #             "pre_tags": ["<b>"],
+        #             "post_tags": ["</b>"],
+        #         }
+        #     },
+        # }
 
 
-class Query(ObjectType):
-    # import pudb
-
-    # pudb.set_trace()
+class Query(graphene.ObjectType):
     all_messages = ElasticsearchConnectionField(MessageNode)
+
+
+"""
+example query:
+
+query {
+  allMessages(
+		search:{query:"Thompson"}
+        # filter: {labels: {terms: ["PERSON", "MONEY"]}}
+  ) {
+    edges {
+      node {
+        msgFrom
+        # msgSubject
+        # msgBody
+        directory
+        labels
+        collection
+      }
+    }
+  }
+}
+"""
