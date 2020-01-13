a9eec62f23c79de81a0c9853c3cf6b286aa46783
Jeremy Gibson <jgibson@caktusgroup.com>
2020-01-13 14:31:35 -0500

    [RATOM-50] Comment out future work

--- deployment/ci_deployment.sh
+++ deployment/ci_deployment.sh
@@ -3,7 +3,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 echo "Pushing images to Docker Hub"
 ansible-playbook deployment/docker-hub.yaml -vv
 
-if ["$TRAVIS_BRANCH" == "staging"];
-then
-    echo "Deploying to staging"
-fi
+# if ["$TRAVIS_BRANCH" == "staging"];
+# then
+#     echo "Deploying to staging"
+# fi
