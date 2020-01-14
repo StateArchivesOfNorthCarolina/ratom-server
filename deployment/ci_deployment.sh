73d6c37ec9b235a4bf8e31361717d79ecbc0103a
Colin Copeland <copelco@caktusgroup.com>
2020-01-14 14:19:21 -0500

    switch names of env variables

--- deployment/ci_deployment.sh
+++ deployment/ci_deployment.sh
@@ -10,22 +10,23 @@ ansible-playbook -i deployment/inventory.yaml deployment/docker-hub.yaml -vv
     echo "Deploying to staging"
 
     cd ./deployment
-    # K8S_AUTH_SSL_CA_CERT=./k8s-ca-cert
+    # KUBE_AUTH_SSL_CA_CERT_DATA=./k8s-ca-cert
 
-    if [ -z "$K8S_AUTH_CREATE_CONFIG" ]; then
+    if [ -z "$KUBE_AUTH_CREATE_CONFIG" ]; then
         echo "Using default kube config"
     else
         echo "Using kube config from environment"
-        [ -z "$K8S_AUTH_SSL_CA_CERT_DATA" ] && echo "Missing K8S_AUTH_SSL_CA_CERT_DATA var" && exit 1
-        # echo $K8S_AUTH_SSL_CA_CERT_DATA | base64 --decode > $K8S_AUTH_SSL_CA_CERT
-        kubectl config set-cluster $K8S_AUTH_CONTEXT-cluster --server=$K8S_AUTH_HOST #--certificate-authority=$K8S_AUTH_SSL_CA_CERT --embed-certs=true
-        kubectl config set clusters.microk8s-cluster.certificate-authority-data $K8S_AUTH_SSL_CA_CERT_DATA
-        kubectl config set-context $K8S_AUTH_CONTEXT --cluster=$K8S_AUTH_CONTEXT-cluster --user=$K8S_AUTH_USERNAME
-        kubectl config set-credentials $K8S_AUTH_USERNAME --username=$K8S_AUTH_USERNAME --password=$K8S_AUTH_PASSWORD
-        kubectl config use-context $K8S_AUTH_CONTEXT
+        [ -z "$KUBE_AUTH_SSL_CA_CERT_DATA" ] && echo "Missing KUBE_AUTH_SSL_CA_CERT_DATA var" && exit 1
+        # echo $KUBE_AUTH_SSL_CA_CERT_DATA | base64 --decode > $KUBE_AUTH_SSL_CA_CERT_DATA
+        kubectl config set-cluster $KUBE_AUTH_CONTEXT-cluster --server=$KUBE_AUTH_HOST #--certificate-authority=$K8S_AUTH_SSL_CA_CERT --embed-certs=true
+        kubectl config set clusters.microk8s-cluster.certificate-authority-data $KUBE_AUTH_SSL_CA_CERT_DATA
+        kubectl config set-context $KUBE_AUTH_CONTEXT --cluster=$KUBE_AUTH_CONTEXT-cluster --user=$KUBE_AUTH_USERNAME
+        kubectl config set-credentials $KUBE_AUTH_USERNAME --username=$KUBE_AUTH_USERNAME --password=$KUBE_AUTH_PASSWORD
+        kubectl config use-context $KUBE_AUTH_CONTEXT
     fi
 
     kubectl config view
+    kubectl cluster-info
     ansible-galaxy install -r requirements.yaml
     ansible-playbook deploy.yaml -l ratom-staging -vvvv
 
