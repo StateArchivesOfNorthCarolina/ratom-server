7082df2645aab464daf5d74f4c6814500b37353b
Colin Copeland <copelco@caktusgroup.com>
2020-01-14 14:34:06 -0500

    remove old way of setting ca cert

--- deployment/ci_deployment.sh
+++ deployment/ci_deployment.sh
@@ -8,17 +8,14 @@ ansible-playbook -i deployment/inventory.yaml deployment/docker-hub.yaml -vv
 # then
 
     echo "Deploying to staging"
-
     cd ./deployment
-    # KUBE_AUTH_SSL_CA_CERT_DATA=./k8s-ca-cert
 
     if [ -z "$KUBE_AUTH_CREATE_CONFIG" ]; then
         echo "Using default kube config"
     else
         echo "Using kube config from environment"
         [ -z "$KUBE_AUTH_SSL_CA_CERT_DATA" ] && echo "Missing KUBE_AUTH_SSL_CA_CERT_DATA var" && exit 1
-        # echo $KUBE_AUTH_SSL_CA_CERT_DATA | base64 --decode > $KUBE_AUTH_SSL_CA_CERT_DATA
-        kubectl config set-cluster $KUBE_AUTH_CONTEXT-cluster --server=$KUBE_AUTH_HOST #--certificate-authority=$K8S_AUTH_SSL_CA_CERT --embed-certs=true
+        kubectl config set-cluster $KUBE_AUTH_CONTEXT-cluster --server=$KUBE_AUTH_HOST
         kubectl config set clusters.microk8s-cluster.certificate-authority-data $KUBE_AUTH_SSL_CA_CERT_DATA
         kubectl config set-context $KUBE_AUTH_CONTEXT --cluster=$KUBE_AUTH_CONTEXT-cluster --user=$KUBE_AUTH_USERNAME
         kubectl config set-credentials $KUBE_AUTH_USERNAME --username=$KUBE_AUTH_USERNAME --password=$KUBE_AUTH_PASSWORD
