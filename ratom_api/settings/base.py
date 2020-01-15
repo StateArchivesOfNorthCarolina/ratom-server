007c0c15459d6e2044f1a7a5e49930190b0f84ce
Michael Ashton <michael.ashton@caktusgroup.com>
2020-01-15 13:35:26 -0500

    sets default pagination server side

--- ratom_api/settings/base.py
+++ ratom_api/settings/base.py
@@ -69,12 +69,14 @@ ELASTICSEARCH_INDEX_NAMES = {
     "ratom.documents.message": "message",
 }
 
+PAGINATION_PAGE_SIZE = 4
+
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 100,
+    "PAGE_SIZE": PAGINATION_PAGE_SIZE,
     "ORDERING_PARAM": "ordering",
 }
 
