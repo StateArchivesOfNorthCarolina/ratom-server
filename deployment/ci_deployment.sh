934ad8d4b969e17e28fbf3b41b842550ccd6f355
Colin Copeland <copelco@caktusgroup.com>
2020-01-15 17:23:57 -0500

    pull vault pass from env

--- deployment/ci_deployment.sh
+++ deployment/ci_deployment.sh
@@ -27,5 +27,6 @@ else
     kubectl config use-context $KUBE_AUTH_CONTEXT
 fi
 
+echo $VAULT_PASS > .vault_pass
 ansible-galaxy install -r requirements.yaml
 ansible-playbook deploy.yaml -l ratom-staging -e k8s_container_image_tag=$TAG
