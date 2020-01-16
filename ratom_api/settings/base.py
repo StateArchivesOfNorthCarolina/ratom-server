74b12d0d3e01369595f537fcef5a551dc94ccfb6
Colin Copeland <copelco@caktusgroup.com>
2020-01-16 16:29:18 -0500

    allow session auth to be used on browsable api

--- ratom_api/settings/base.py
+++ ratom_api/settings/base.py
@@ -69,6 +69,7 @@ PAGINATION_PAGE_SIZE = 4
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": PAGINATION_PAGE_SIZE,
