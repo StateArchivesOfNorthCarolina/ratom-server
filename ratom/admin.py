10648ca688f13d4e1d441e7f44f67bd57d39ad91
Colin Copeland <copelco@caktusgroup.com>
2020-01-16 14:37:09 -0500

    re-enable admin options

--- ratom/admin.py
+++ ratom/admin.py
@@ -17,8 +17,7 @@ class MessageAdmin(admin.ModelAdmin):
         "subject",
         "account",
     )
-    # list_filter = ("sent_date", "collection")
-    # search_fields = ("msg_body",)
-    # date_hierarchy = "sent_date"
-    # ordering = ("-sent_date",)
-    # raw_id_fields = ("processor",)
+    list_filter = ("sent_date", "account")
+    search_fields = ("msg_body",)
+    date_hierarchy = "sent_date"
+    ordering = ("-sent_date",)
