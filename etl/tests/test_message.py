af597674952c560dd348488ccd11e395a7e6b30c
Colin Copeland <copelco@caktusgroup.com>
2020-01-22 09:30:46 -0500

    simplify test

--- etl/tests/test_message.py
+++ etl/tests/test_message.py
@@ -7,12 +7,11 @@ def test_valid_delivery_time(test_archive, archive_msg):
     """Normal datetime should result in valid form"""
     archive_msg.delivery_time = dt.datetime(2019, 1, 1, 3, 0, 0)
     form = ArchiveMessageForm(archive=test_archive, archive_msg=archive_msg)
-    assert form.is_valid()
+    assert form.is_valid(), form.errors
 
 
 def test_body__null_chars(test_archive, archive_msg):
-    archive_msg.plain_text_body = "Hi\x00there"
-    test_archive.format_message.return_value = archive_msg.plain_text_body
+    test_archive.format_message.return_value = "Hi\x00there"
     form = ArchiveMessageForm(archive=test_archive, archive_msg=archive_msg)
     assert form.is_valid(), form.errors
     assert form.cleaned_data["body"] == "Hithere"
