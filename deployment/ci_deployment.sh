44ec8c0e35f7eecd88bd1165b1920154b5d1454a
Colin Copeland <copelco@caktusgroup.com>
2020-02-04 16:56:54 -0500

    remove microk8s config from CI deploy

--- deployment/ci_deployment.sh
+++ deployment/ci_deployment.sh
@@ -15,18 +15,6 @@ docker push $DOCKERHUB_REPO:latest
 echo "Deploying to staging"
 cd ./deployment
 
-if [ -z "$KUBE_AUTH_CREATE_CONFIG" ]; then
-    echo "Using default kube config"
-else
-    echo "Using kube config from environment"
-    [ -z "$KUBE_AUTH_SSL_CA_CERT_DATA" ] && echo "Missing KUBE_AUTH_SSL_CA_CERT_DATA var" && exit 1
-    kubectl config set-cluster $KUBE_AUTH_CONTEXT-cluster --server=$KUBE_AUTH_HOST
-    kubectl config set clusters.microk8s-cluster.certificate-authority-data $KUBE_AUTH_SSL_CA_CERT_DATA
-    kubectl config set-context $KUBE_AUTH_CONTEXT --cluster=$KUBE_AUTH_CONTEXT-cluster --user=$KUBE_AUTH_USERNAME
-    kubectl config set-credentials $KUBE_AUTH_USERNAME --username=$KUBE_AUTH_USERNAME --password=$KUBE_AUTH_PASSWORD
-    kubectl config use-context $KUBE_AUTH_CONTEXT
-fi
-
 echo $VAULT_PASS > .vault_pass
 ansible-galaxy install -r requirements.yaml
 ansible-playbook deploy.yaml -l ratom-staging -e k8s_container_image_tag=$TAG
