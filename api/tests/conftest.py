22fcd2cb3e5461d9767bda23639e2b67fbdf13ba
Colin Copeland <copelco@caktusgroup.com>
2020-02-07 14:04:48 -0500

    update fixtures

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -4,5 +4,11 @@ from rest_framework.test import APIClient
 
 
 @pytest.fixture
-def api_client():
-    return APIClient()
+def api_client_anon():
+    yield APIClient()
+
+
+@pytest.fixture
+def api_client(api_client_anon, user):
+    api_client_anon.force_authenticate(user=user)
+    yield api_client_anon
--- conftest.py
+++ conftest.py
@@ -6,16 +6,17 @@ from core.tests import factories
 @pytest.fixture
 def account():
     """ratom.core.models.Account instance"""
-    return factories.AccountFactory()
+    yield factories.AccountFactory()
 
 
 @pytest.fixture
 def ratom_file(account):
     """ratom.core.models.File instance"""
-    return factories.FileFactory(account=account)
+    yield factories.FileFactory(account=account)
 
 
 @pytest.fixture
-def user():
+def user(django_db_blocker):
     """ratom.core.models.File instance"""
-    return factories.UserFactory()
+    with django_db_blocker.unblock():
+        yield factories.UserFactory()
