81dc559eae1e8fa1e5cb3faad9d236c6d739ee41
Colin Copeland <copelco@caktusgroup.com>
2020-02-10 11:50:42 -0500

    add test for open

--- etl/tests/test_providers.py
+++ etl/tests/test_providers.py
@@ -59,3 +59,15 @@ def test_azure_blob__size_exists(cloud_provider, pst_blob):
 def test_azure_blob__size_not_exists(cloud_provider, pst_blob):
     pst_blob.name = "i-dont-exist.pst"
     assert not cloud_provider.file_size
+
+
+def test_azure_blob__open(cloud_provider, tmp_path):
+    with mock.patch("etl.providers.base.PffArchive") as archive_mock:
+        pst_file = tmp_path / "name.pst"
+        pst_file.write_bytes(b"Binary file contents")
+        cloud_provider._client.download_blob = mock.Mock()
+        cloud_provider._client.download_blob.return_value.readall.return_value = (
+            pst_file.read_bytes()
+        )
+        cloud_provider.open()
+        assert archive_mock.called_once
