883fa3b84b8097d2ff9cf204e96abdccbd3fe815
Colin Copeland <copelco@caktusgroup.com>
2020-02-13 16:34:42 -0500

    mock ES registry in all tests

--- api/tests/test_signals.py
+++ api/tests/test_signals.py
@@ -1,10 +1,10 @@
 import pytest
-from unittest import mock
 
 
 @pytest.mark.django_db
-def test_message_audit_update_triggers_message_reindex(ratom_message_audit):
+def test_message_audit_update_triggers_message_reindex(
+    ratom_message_audit, mock_core_registry
+):
     ratom_message_audit.processed = False
-    with mock.patch("core.signals.registry.update") as mock_registry_update:
-        ratom_message_audit.save()
-        mock_registry_update.assert_called_with(ratom_message_audit.message)
+    ratom_message_audit.save()
+    mock_core_registry.update.assert_called_with(ratom_message_audit.message)
