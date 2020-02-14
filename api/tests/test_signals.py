cdfcc042566e2d05b3e00cceb3e04283ffd84b5c
Colin Copeland <copelco@caktusgroup.com>
2020-02-14 11:39:29 -0500

    test that new audits don't trigger signal

--- api/tests/test_signals.py
+++ api/tests/test_signals.py
@@ -1,4 +1,5 @@
 import pytest
+from core.tests import factories
 
 
 @pytest.mark.django_db
@@ -8,3 +9,10 @@ def test_message_audit_update_triggers_message_reindex(
     ratom_message_audit.processed = False
     ratom_message_audit.save()
     mock_core_registry.update.assert_called_with(ratom_message_audit.message)
+
+
+@pytest.mark.django_db
+def test_audit_created_no_signal(mock_core_registry):
+    """Verify new audits don't trigger an index update (since message will already do it)."""
+    factories.MessageAuditFactory()
+    mock_core_registry.update.assert_not_called()
