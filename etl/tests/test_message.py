94da1ec5a6b14df4a9d06c3ec6f2203fad2549eb
Michael Ashton <michael.ashton@caktusgroup.com>
2020-02-24 12:55:32 -0500

    [RATOM-78] remove script tags from html emails, adds bs4 to requirements

--- etl/tests/test_message.py
+++ etl/tests/test_message.py
@@ -18,6 +18,20 @@ def test_body__null_chars(test_archive, archive_msg):
     assert form.cleaned_data["body"] == "Hithere"
 
 
+def test_body__clean_html(test_archive, archive_msg):
+    test_archive.format_message.return_value = """
+        <body>
+            <h1>I'm fine</h1>
+            <script>I'm bad</script>
+        </body>
+    """
+    form = ArchiveMessageForm(archive=test_archive, archive_msg=archive_msg)
+    assert form.is_valid(), form.errors
+    assert "<script>I'm bad</script>" not in form.cleaned_data["body"]
+    assert "I'm bad" not in form.cleaned_data["body"]
+    assert "I'm fine" in form.cleaned_data["body"]
+
+
 def test_sent_date__make_aware_fails(test_archive, archive_msg):
     """form should still be valid if make_aware fails."""
     with mock.patch("etl.message.forms.make_aware", side_effect=Exception):
