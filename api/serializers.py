02795bc4c327125a7fc5784871be040d2337fb11
Jeremy Gibson <jgibson@caktusgroup.com>
2020-03-04 17:04:55 -0500

    [RATOM-195] Adds a more complex label structure

--- api/serializers.py
+++ api/serializers.py
@@ -74,8 +74,19 @@ class AccountSerializer(serializers.ModelSerializer):
                 "%Y-%m-%d", as_string=False
             ),
             "account_status": instance.get_account_status(),
-            "labels": Label.objects.all().values("type", "name"),
+            "labels": self.get_label_suggestions(),
         }
+
+    def get_label_suggestions(self):
+        suggestions = []
+        for l in Label.objects.all():
+            suggestions.append(
+                {
+                    "section_type": l.type,
+                    "label_suggestions": {"id": l.pk, "name": l.name},
+                }
+            )
+        return suggestions
 
 
 class MessageAuditSerializer(serializers.ModelSerializer):
