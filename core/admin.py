977c3f6b9859f4e6c180d91b8f5b1e4f759b4826
Colin Copeland <copelco@caktusgroup.com>
2020-03-23 12:22:04 -0400

    [RATOM-165] add blank=True to optional fields; add addl metadata to make admin more useful

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.db.models import Count
+from django.template.defaultfilters import filesizeformat
 from core import models as ratom
 
 admin.site.register(ratom.Account)
@@ -64,10 +66,10 @@ class MessageAuditAdmin(admin.ModelAdmin):
         "updated_by",
     )
     list_select_related = ("message",)
-    list_filter = ("is_record", "processed")
+    list_filter = ("is_record", "processed", "message__account")
     search_fields = ("message__body", "pk")
     date_hierarchy = "date_processed"
-    ordering = ("-message__sent_date",)
+    ordering = ("-pk",)
 
 
 @admin.register(ratom.File)
@@ -77,12 +79,29 @@ class FileAdmin(admin.ModelAdmin):
         "account",
         "filename",
         "reported_total_messages",
+        "imported_total_messages",
+        "message_errors",
+        "total_file_size",
         "date_imported",
         "import_status",
     )
     list_filter = ("import_status", "account")
     ordering = ("-date_imported",)
     search_fields = ("filename", "pk")
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(_message_count=Count("message", distinct=True),)
+        return queryset
+
+    def imported_total_messages(self, obj):
+        return obj._message_count
+
+    def message_errors(self, obj):
+        return len(obj.errors) if obj.errors else ""
+
+    def total_file_size(self, obj):
+        return filesizeformat(obj.file_size)
 
 
 @admin.register(ratom.Label)
